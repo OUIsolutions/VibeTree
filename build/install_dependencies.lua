24362b928af1f6f874e4c32932a89cd75ddf3486
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-07-16 12:18:25 -0300

    att

--- build/install_dependencies.lua
+++ build/install_dependencies.lua
@@ -1,13 +1,13 @@
 function Install_dependencies()
     local hasher = darwin.dtw.newHasher()
     hasher.digest_folder_by_content("dependencies")
-    local TARGET_HASH = "90291b5144697288b1221f0baa348e36f810ff626445cda5ff95ba3f3a173789"
+    local TARGET_HASH = "b576c55b53ff050ffcb5a85507ebbe8b6b77cd53e0d7b28e06798c7f88418902"
     if hasher.get_value() == TARGET_HASH then
         return
     end
     local comands = {
         "mkdir -p dependencies",
-        "curl -L https://github.com/OUIsolutions/DoTheWorld/releases/download/10.1.1/doTheWorldOne.c -o dependencies/doTheWorldOne.c",
+        "curl -L https://github.com/OUIsolutions/DoTheWorld/releases/download/10.2.0/doTheWorldOne.c -o dependencies/doTheWorldOne.c",
         "curl -L https://github.com/OUIsolutions/C-argv-parser/releases/download/0.0.2/CArgvParseOne.c -o dependencies/CArgvParseOne.c",
     }
     for _, command in ipairs(comands) do
