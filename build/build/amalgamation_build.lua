48f45aa6b4a069f5f1c0c44ad969f084ed9d2300
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-07-16 09:37:02 -0300

    att

--- build/build/amalgamation_build.lua
+++ build/build/amalgamation_build.lua
@@ -5,12 +5,7 @@ function amalgamation_build()
     end
     alreay_amalamated_done = true
 
-    local prev = [[
-        #ifndef PATH_MAX
-    #define PATH_MAX 4096
-    #endif
-    ]]
-    local runtime = prev.. darwin.camalgamator.generate_amalgamation("src/main.c")
-    darwin.dtw.write_file("release/amalgamation.c", runtime)
+    local amalgamation =  darwin.camalgamator.generate_amalgamation("src/main.c")
+    darwin.dtw.write_file("release/amalgamation.c", amalgamation)
 
 end
